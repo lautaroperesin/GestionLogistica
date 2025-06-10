60fdd1a3fc561b0f1a85b5b244ed8c88bf2acbb6
Lautaro Peresin <lautaroperesin@gmail.com>
2025-06-10 18:12:04 -0300

    Se agregaron validaciones en el CreateUbicacionDto

--- DTOs/Ubicacion/CreateUbicacionDto.cs
+++ DTOs/Ubicacion/CreateUbicacionDto.cs
@@ -1,8 +1,14 @@
-﻿namespace GestionLogisticaBackend.DTOs.Ubicacion
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionLogisticaBackend.DTOs.Ubicacion
 {
     public class CreateUbicacionDto
     {
+        [Required(ErrorMessage = "La dirección es requerida")]
+        [StringLength(100, ErrorMessage = "La dirección no puede exceder los 100 caracteres")]
         public string Direccion { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La localidad es requerida")]
         public int IdLocalidad { get; set; }
     }
 }
