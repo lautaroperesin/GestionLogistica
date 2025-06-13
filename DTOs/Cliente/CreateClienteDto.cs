d3b8e79bc8aead33188804a1aafa5c7bf7886827
Lautaro Peresin <lautaroperesin@gmail.com>
2025-06-13 17:44:18 -0300

    Arreglo en las inconsistencias en los DTOs

--- DTOs/Cliente/CreateClienteDto.cs
+++ DTOs/Cliente/CreateClienteDto.cs
@@ -4,17 +4,17 @@ namespace GestionLogisticaBackend.DTOs.Cliente
 {
     public class CreateClienteDto
     {
-        [Required(ErrorMessage = "El nombre es obligatorio")]
-        [StringLength(100, ErrorMessage = "El nombre no puede exceder 100 caracteres")]
+        [Required(ErrorMessage = "El nombre del cliente es requerido")]
+        [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres")]
         public string Nombre { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "El email es obligatorio")]
+        [Required(ErrorMessage = "El email es requerido")]
         [EmailAddress(ErrorMessage = "El formato del email no es válido")]
-        [StringLength(150, ErrorMessage = "El email no puede exceder 150 caracteres")]
+        [StringLength(100, ErrorMessage = "El email no puede exceder los 100 caracteres")]
         public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "El teléfono es requerido")]
-        [StringLength(20, ErrorMessage = "El teléfono no puede exceder 20 caracteres")]
+        [StringLength(100, ErrorMessage = "El teléfono no puede exceder los 100 caracteres")]
         public string? Telefono { get; set; }
     }
 }
